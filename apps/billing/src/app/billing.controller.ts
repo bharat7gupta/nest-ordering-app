0382e9a890209b11925986eb3cb8d52e76d0060b
Bharat Gupta <bharat.gupta@navi.com>
2023-04-16 12:06:06 +0530

    ack rmq message

--- apps/billing/src/app/billing.controller.ts
+++ apps/billing/src/app/billing.controller.ts
@@ -2,10 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 
 import { BillingService } from './billing.service';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { RmqService } from '@app/common';
 
 @Controller()
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  constructor(
+    private readonly billingService: BillingService,
+    private readonly rmqService: RmqService
+  ) {}
 
   @Get()
   getData() {
@@ -15,6 +19,7 @@ export class BillingController {
   @EventPattern('ORDER_CREATED')
   handleOrderCreated(@Payload() data, @Ctx() context: RmqContext) {
     console.log('billing', data);
-    this.billingService.bill(data)
+    this.billingService.bill(data);
+    this.rmqService.ack(context);
   }
 }
