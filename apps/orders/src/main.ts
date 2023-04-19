ac37be1276955e975b2f21c74f617a35db4f4292
Bharat Gupta <bharat.gupta@navi.com>
2023-04-19 16:36:23 +0530

    added swagger

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -8,6 +8,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { OrdersModule } from './app/orders.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
@@ -18,6 +19,18 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  /** Api Documentation */
+  const config = new DocumentBuilder()
+    .setTitle('Orders Api')
+    .setDescription('The orders API for the ordering app')
+    .setVersion('1.0')
+    .addTag('orders')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(port);
 
   Logger.log(
